cbb01b0e2f091561f81a8609289a163a55c85952
giophub <25926390+giophub@users.noreply.github.com>
2018-09-26 11:59:48 +0200

    save the SOAP response on filesystem.

--- src/main/java/com/giophub/main/Main.java
+++ src/main/java/com/giophub/main/Main.java
@@ -9,7 +9,38 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String argv[]) {
+    public static void main(String argv[]) throws Exception {
+        /*if (argv.length != 3) {
+            System.out.println("Usage: java -classpath <classpath> [-Dorg.apache.commons.logging.simplelog.defaultlog=<loglevel>] PostSOAP <url> <soapaction> <filename>]");
+            System.out.println("<classpath> - must contain the commons-httpclient.jar and commons-logging.jar");
+            System.out.println("<loglevel> - one of error, warn, info, debug, trace");
+            System.out.println("<url> - the URL to post the file to");
+            System.out.println("<soapaction> - the SOAP action header value");
+            System.out.println("<filename> - file to post to the URL");
+            System.out.println();
+            System.exit(1);
+        }
+        String uri = argv[0];           // Get target URI
+        String soapAction = argv[1]; // Get SOAP action
+        String fileName = argv[2];      // Get file to be posted
+
+        BufferedReader buffer = new FileLoader().load(fileName).asBufferedReader();
+        String soapXmlFile = new Parser().asString(buffer);
+        ApacheSoapClient apacheSoapClient = new ApacheSoapClient(uri, soapAction, soapXmlFile);
+*/
+
+
+
+        /**
+         * TODO 1 :*: get CMD arguments like: java -jar web-client arg1 arg2 argN
+         * TODO 2 :*: load the file name and read it
+         * TODO 3 :*: call the web service / application
+         * TODO 4 :*: get the response / content response
+         * TODO 5 :*: save the content response on file / logfile
+         * TODO 6 :: track the response execution time
+         * TODO 7 :: schedule to run this application N-times with spring scheduler
+         * */
+
 
         BufferedReader buffer = new FileLoader().load("test/basic-xml-example.xml").asBufferedReader();
 
--- src/main/java/com/giophub/web/client/ApacheSoapClient.java
+++ src/main/java/com/giophub/web/client/ApacheSoapClient.java
@@ -11,7 +11,10 @@ import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
 public class ApacheSoapClient {
     private static final Logger LOGGER = LoggerFactory.getLogger(ApacheSoapClient.class);
@@ -32,6 +35,7 @@ public class ApacheSoapClient {
             httpPost.addHeader("Content-Type", "text/xml");
             httpPost.addHeader("Accept", "text/xml");
             httpPost.addHeader("SOAPAction", soapAction);
+//            httpPost.addHeader("Content-Disposition", "attachment; filename=\"export.xml\"");
 
             // Execute and get the response.
             CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
@@ -40,9 +44,25 @@ public class ApacheSoapClient {
                 HttpEntity entity = httpResponse.getEntity();
 
                 // convert content response to string
-                LOGGER.debug("Start to print the content response");
-                String sContentResponse = EntityUtils.toString(entity);
-                System.out.println(sContentResponse);
+//                LOGGER.debug("Start to print the content response");
+//                String sContentResponse = EntityUtils.toString(entity);
+//                System.out.println(sContentResponse);
+
+                // get disposition from response header
+                /*String disposition = httpResponse.getFirstHeader("Content-Disposition").getValue();
+                LOGGER.debug("Response.getFirstHeader name: {}", disposition);*/
+
+                // get file name from response
+                String fileName;
+//                fileName = disposition.replaceFirst("(?i)^.*filename=\"([^\"]+)\".*$", "$1");
+                fileName = "content-response.xml";
+                fileName = "C:\\_logs\\myDIR\\file\\" + fileName;
+                LOGGER.debug("Output filename: {}", fileName);
+
+                // write the response on filesystem
+                FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+                entity.writeTo(fileOutputStream);
+                fileOutputStream.close();
 
                 // and ensure it is fully consumed
                 EntityUtils.consume(entity);
