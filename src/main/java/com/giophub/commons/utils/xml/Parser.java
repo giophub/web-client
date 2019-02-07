6da7c12d18d1a9cfba0c390990c0aedaa7e0af1e
pergio <Giovanni.PERTA@intrasoft-intl.com>
2019-02-07 15:07:38 +0100

    some improvements to the Parser.

--- src/main/java/com/giophub/commons/utils/xml/Parser.java
+++ src/main/java/com/giophub/commons/utils/xml/Parser.java
@@ -1,16 +1,55 @@
 package com.giophub.commons.utils.xml;
 
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class Parser {
     private static final Logger LOGGER = LoggerFactory.getLogger(Parser.class);
 
     public void Parser() {
+    }
+
+    public static Document convertToDOM(String message) {
+        return convertToDOM( IOUtils.toInputStream(message, Charsets.UTF_8) );
+    }
+
+    public static Document convertToDOM(InputStream is) {
+        Document dom = null;
+
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            dbf.setValidating(false);
+            dbf.setIgnoringComments(false);
+            dbf.setIgnoringElementContentWhitespace(true);
+            dbf.setNamespaceAware(true);
+            // dbf.setCoalescing(true);
+            // dbf.setExpandEntityReferences(true);
+
+            DocumentBuilder builder = dbf.newDocumentBuilder();
+            dom = builder.parse(is);
+            dom.getDocumentElement().normalize(); // normalize the document
+
+        } catch (ParserConfigurationException e) {
+            LOGGER.error("Document builder factory exception: {}", e.getMessage());
+        } catch (SAXException e) {
+            LOGGER.error("Error on parsing the XML Document from Input Stream: {}", e.getMessage());
+        } catch (IOException e) {
+            LOGGER.error("I/O Exception on parsing XML Document: {}", e.getMessage());
+        }
+
+        return dom;
     }
 
     public void asBufferedReader(BufferedReader bufferedReader) {
